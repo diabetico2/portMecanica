2fb67b13e6d48ae0489adcd8b874702d0dcc9983
Allan José <allanjosepereira18@gmail.com>
2025-03-21 19:32:55 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
